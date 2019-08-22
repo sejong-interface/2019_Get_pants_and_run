a9164361e1bb4106eee183abc83a4406895873ac
WhiskeyHolic <kysook000117@gmail.com>
2019-08-22 19:33:46 +0900

    김태균 190816과제

---
+++ WhiskyHolic/UnityTest/Test/Assets/Scripts/CameraWork.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraWork : MonoBehaviour
+{
+    GameObject Sphere;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Sphere = GameObject.Find("Sphere");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Sphere.transform.position.x - 1, Sphere.transform.position.y - 1, Sphere.transform.position.z - 8);
+    }
+}
---
+++ WhiskyHolic/UnityTest/Test/Assets/Scripts/Flat.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flat : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float zRotation = transform.localEulerAngles.z;
+
+        zRotation = Input.GetAxis("Horizontal") + transform.localEulerAngles.z;
+        transform.localEulerAngles = new Vector3(0, -44, zRotation);
+
+        if(Input.touchCount > 0 || Input.GetMouseButton(0))
+        {
+            if (Input.mousePosition.x <= Screen.width / 2) transform.localEulerAngles = new Vector3(transform.localEulerAngles.x + 10, transform.localEulerAngles.y, transform.localEulerAngles.z);
+            else if (Input.mousePosition.x > Screen.width / 2) transform.localEulerAngles = new Vector3(transform.localEulerAngles.x - 10, transform.localEulerAngles.y, transform.localEulerAngles.z);
+        }
+    }
+}
