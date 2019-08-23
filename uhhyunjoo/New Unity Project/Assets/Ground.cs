6372a6e9b151a107a0a0a97b6d676df97290ba3d
alro923 <alro923@naver.com>
2019-08-23 16:03:24 +0900

    키 입력 받아서 오브젝트 이동

---
+++ uhhyunjoo/New Unity Project/Assets/Ground.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float zRotation = transform.localEulerAngles.z;
+        zRotation = zRotation - Input.GetAxis("Horizontal");
+        transform.localEulerAngles = new Vector3(10, 0, zRotation);
+    }
+}
