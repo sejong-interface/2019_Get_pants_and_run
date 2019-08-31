bf3779dd28b93fb85775567dff6a441f6aba012d
KimChaeWon1230 <cyn1025@naver.com>
2019-09-01 00:10:31 +0900

    이번 과제입니다.

---
+++ Unity/Assets/Ball.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ball : MonoBehaviour
+{
+    float startingPoint;
+    public float distance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPoint = transform.position.z;//시작지점 z포인트값 저장
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance;
+        float timeCount = 0;
+        
+        float newZPosition = transform.position.z + 0.1f;//z방향으로 쭉 움직이기
+        
+        
+        distance = transform.position.z - startingPoint;
+        GameManager gmComponent = GameObject.Find("GameManager").GetComponent<GameManager>();
+
+        //Debug.Log(distance);
+        //이동한 거리 숫자로 보여주기
+
+
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            //GetComponent<Rigidbody>().AddForce(Vector3.up * 300);//스페이스 입력받으면 점프
+            Rigidbody ballRigid;
+            ballRigid = gameObject.GetComponent<Rigidbody>();
+            ballRigid.AddForce(Vector3.up * 1000);
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            timeCount += Time.deltaTime;
+            if (timeCount < 10)
+            {
+                Rigidbody ballRigid;
+                ballRigid = gameObject.GetComponent<Rigidbody>();
+                ballRigid.AddForce(Vector3.forward * 100);
+            }
+            if (timeCount > 10)
+            {
+                timeCount = 0;
+            }
+        }
+        else
+        {
+            transform.position = new Vector3(0, 1.34f, newZPosition);
+        }
+        
+    }
+}
---
+++ Unity/Assets/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GameManager : MonoBehaviour
+{
+    public int coinCount = 0;
+    public float distance;
+    public Text coinText;
+    public Text distanceText;
+    // Start is called before the first frame update
+    void GetCoin()
+    {
+        coinCount++;
+        coinText.text = coinCount + "개";
+        Debug.Log("동전: " + coinCount);
+    }
+    
+    void distancemeter()
+    {
+        distanceText.text = "달린거리 : " + distance + "m";
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
